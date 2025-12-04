a0a966e12a724b65b96c13009d082b1b2619103f
Charles-349 <wamahiucharles123@gmail.com>
2025-12-04 21:20:24 +0300

    deploying on render

--- src/Drizzle/db.ts
+++ src/Drizzle/db.ts
@@ -1,23 +1,10 @@
-  import "dotenv/config"
 
-  import { drizzle } from "drizzle-orm/node-postgres"
-  import { Client } from "pg"
-  import * as schema from "./schema"
+import "dotenv/config"
+import {drizzle} from "drizzle-orm/neon-http"
+import * as schema from "./schema"
+import { neon } from "@neondatabase/serverless"
 
-  export const client = new Client({
-      connectionString: process.env.Database_URL as string
-  })
-
-  const main = async () => {
-      await client.connect()
-  }
-  main().then(() => {
-      console.log("Connected to the database")
-  }).catch((error) => {
-      console.error("Error connecting to the database:", error)
-  })
-
-
-  const db = drizzle(client, { schema, logger: true })
+export const client = neon(process.env.Database_URL!)
+  const db = drizzle(client, { schema, logger: false })
 
   export default db
--- src/Drizzle/migrate.ts
+++ src/Drizzle/migrate.ts
@@ -1,16 +1,27 @@
 import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
-import db, { client } from "./db"
+import pkg from "pg";
 
-async function migration() {
-    console.log("......Migrations Started......");
-    await migrate(db, { migrationsFolder: __dirname + "/migrations" });
+const { Client } = pkg;
+
+const client = new Client({
+  connectionString: process.env.Database_URL,
+});
+
+async function main() {
+  try {
+    console.log("Running migrations...");
+    await client.connect();
+    const db = drizzle(client);
+    await migrate(db, { migrationsFolder: "./src/Drizzle/migrations" });
+    console.log("Migrations completed successfully!");
+  } catch (err) {
+    console.error(" Migration failed:", err);
+  } finally {
     await client.end();
-    console.log("......Migrations Completed......");
-    process.exit(0); // 0 means success
+    process.exit(0);
+  }
 }
 
-migration().catch((error) => {
-    console.error("Migration failed:", error);
-    process.exit(1); // 1 means an error occurred
-});
+main();
